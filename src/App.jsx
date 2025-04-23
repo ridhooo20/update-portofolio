e898bb05af8b3ff58a854b738b6131e6751b1a73
ridhomulya <githubridho@gmail.com>
2025-04-23 15:35:15 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import "./style.css";
 const App = () => {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:p-0 p-0">
-      <div className="max-w-7xl w-full ">
+      <div className="w-full ">
         <Navbar />
         <Hero />
         <About />
